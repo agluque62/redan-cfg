1235900f9ff92b6462e9efc907c06e49b7bd78b9
Vicente Molina Garcia <vmolinagarcia@gmail.com>
2017-03-02 16:23:21 +0100

    Mostrando módulo de verriondo

---
+++ public/javascripts/version.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vjmolina on 2/3/17.
+ */
+
+
+var GetVersion = function() {
+	$('#DivVersion').animate({width: '535px'});
+	$("#AddFormVersion").show();
+	$("#FormVersion").show();
+	
+}
+
+
+function BuildVersionInfo(){
+	
+}
